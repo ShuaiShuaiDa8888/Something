8da568403eeb11a5126ccc97af18654504d9f872
weiShuai <601770050@qq.com>
2018-03-03 14:46:36 +0800

    新增IO流读写代码

---
+++ IO/src/test/java/copyTest/TestOfCopyIO.java
@@ -0,0 +1,16 @@
+package copyTest;
+
+import org.junit.Test;
+
+/**
+ * Created by WS on 2018/3/2.
+ */
+public class TestOfCopyIO {
+
+    private CopyIO copyIO;
+
+    @Test
+    public void Copy(){
+
+    }
+}
